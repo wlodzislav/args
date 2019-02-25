97b9c31fc3dfcfe77d794c367028b26095181fe5
Vladislav Kaminsky <wlodzislav@ya.ru>
2019-02-25 15:14:05 +0400

    Refactor parse

--- args.h
+++ args.h
@@ -128,19 +128,7 @@ namespace args {
 		}
 	}
 
-	template<typename Allocator> // wtf Allocator?
-	void parse(int argc, const char** argv, const std::vector<args::option, Allocator>& options);
-
-	void parse(int argc, const char** argv, const std::initializer_list<args::option>& options) {
-		std::vector<option> options_vec;
-		for (auto&& e : options) {
-			options_vec.push_back(e);
-		}
-		parse(argc, argv, options_vec);
-	}
-
-	template<typename Allocator>
-	void parse(int argc, const char** argv, const std::vector<args::option, Allocator>& options) {
+	void parse(int argc, const char** argv, const std::vector<args::option>& options) {
 		std::map<std::string, std::function<void (const char*)>> global_options;
 		for (auto&& option : options) {
 			if (!option.short_name.empty()) {
@@ -151,32 +139,26 @@ namespace args {
 			}
 		}
 		for (const char** arg = argv + 1; arg < argv + argc; ++arg) {
-			bool short_option = is_short_option(*arg);
-			bool short_option_width_eq_sign_value = is_short_option_width_eq_sign_value(*arg);
-			bool short_grouped_or_with_value = is_short_grouped_or_with_value(*arg);
-			bool short_grouped = is_short_grouped(*arg);
-			bool long_option = is_long_option(*arg);
-			bool long_option_width_eq_sign_value = is_long_option_width_eq_sign_value(*arg);
 			std::string option;
 			std::string value;
 			//INSPECT(*arg);
-			if (short_option) {
+			if (is_short_option(*arg)) {
 				option = std::string(*arg);
 				auto&& next = std::next(arg);
 				if (next != argv + argc && !is_option(*next)) {
 					value = std::string(*next);
 					++arg;
 				}
-			} else if (short_option_width_eq_sign_value) {
+			} else if (is_short_option_width_eq_sign_value(*arg)) {
 				option = std::string(*arg, 2);
 				value = std::string(*arg + 3);
-			} else if (short_grouped_or_with_value) {
-				if (short_grouped) {
+			} else if (is_short_grouped_or_with_value(*arg)) {
+				if (is_short_grouped(*arg)) {
 				} else {
 					option = std::string(*arg, 2);
 					value = std::string(*arg + 2);
 				}
-			} else if (long_option) {
+			} else if (is_long_option(*arg)) {
 				option = std::string(*arg);
 				auto&& next = std::next(arg);
 				if( next != argv + argc && !is_option(*next) ) {
@@ -184,7 +166,7 @@ namespace args {
 					++arg;
 				}
 			}
-			else if (long_option_width_eq_sign_value) {
+			else if (is_long_option_width_eq_sign_value(*arg)) {
 				std::string::size_type eq_pos = std::string(*arg).find("=");
 				option = std::string(*arg, eq_pos);
 				value = std::string(*arg + eq_pos + 1);
@@ -198,6 +180,14 @@ namespace args {
 						std::string("Invalid command line option \"") + *arg + "\".");
 			}
 		}
+	}
+
+	void parse(int argc, const char** argv, const std::initializer_list<args::option>& options) {
+		std::vector<option> options_vec;
+		for (auto&& e : options) {
+			options_vec.push_back(e);
+		}
+		parse(argc, argv, options_vec);
 	}
 }
 
